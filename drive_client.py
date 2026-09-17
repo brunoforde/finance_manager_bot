f0bbf41742be539dbba42666b72046bc6e26771a
Bruno Berlanga <bruno.berlanga@radixeng.com>
2026-09-17 08:37:08 -0300

    add oauth personal acc

--- drive_client.py
+++ drive_client.py
@@ -1,22 +1,39 @@
 import io
-import json
 import os
-from google.oauth2 import service_account
+from google.oauth2.credentials import Credentials
+from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseDownload, MediaFileUpload
 
 SCOPES = ['https://www.googleapis.com/auth/drive']
 
 def get_drive_service():
-    sa_info = json.loads(os.environ["GCP_SA_KEY"])
-    creds = service_account.Credentials.from_service_account_info(
-        sa_info, scopes=SCOPES
+    """Autentica na API do Drive usando OAuth2 em nome do seu usuário pessoal."""
+    client_id = os.environ["GDRIVE_CLIENT_ID"]
+    client_secret = os.environ["GDRIVE_CLIENT_SECRET"]
+    refresh_token = os.environ["GDRIVE_REFRESH_TOKEN"]
+
+    creds = Credentials(
+        None,
+        refresh_token=refresh_token,
+        token_uri="https://oauth2.googleapis.com/token",
+        client_id=client_id,
+        client_secret=client_secret,
+        scopes=SCOPES
     )
+    creds.refresh(Request())
     return build('drive', 'v3', credentials=creds)
 
 def list_files(service, folder_id):
-    query = f"'{folder_id}' in parents and trashed=false"
-    results = service.files().list(q=query, fields="files(id, name)").execute()
+    query = (
+        f"'{folder_id}' in parents and "
+        f"trashed = false and "
+        f"mimeType != 'application/vnd.google-apps.folder'"
+    )
+    results = service.files().list(
+        q=query, 
+        fields="files(id, name, mimeType)"
+    ).execute()
     return results.get('files', [])
 
 def download_file(service, file_id, destination_path):
@@ -37,3 +54,11 @@ def upload_file(service, folder_id, file_path, file_name):
         mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
     )
     service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+
+def move_file(service, file_id, current_folder_id, target_folder_id):
+    service.files().update(
+        fileId=file_id,
+        addParents=target_folder_id,
+        removeParents=current_folder_id,
+        fields='id, parents'
+    ).execute()
